0d7aa05bb06cb2ecbc350dcf43d0cfbca820ab64
Salih Agić <salih.agic.dev@gmail.com>
2025-03-08 21:28:37 +0100

    dart sdk: ^3.6.0

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,20 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Hello World!'),
+      ),
     );
   }
 }
